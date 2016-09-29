0caf95f5ff8da7da8a786af9497fd65ffd13f886
Benoit Plessis <b.plessis@doyousoft.com>
2016-09-30 00:58:30 +0200

    Indent code

--- firmware/leds.c
+++ firmware/leds.c
@@ -1,50 +1,51 @@
 #include <xc.h>
 #include "leds.h"
 
-void setup_leds() {
+// Setup RC2 RC3 RC4 for led operations
+void setup_leds (  ) {
 
-   // Set RA5 and RA4 for led operations
-	
-	ANSELC = 0x00;
-	
-	TRISCbits.TRISC4 = 0 ;
+	ANSELC = 0x00;		// Disable analog use of port C
+	TRISCbits.TRISC4 = 0 ;	// Set ports C pins as output
 	TRISCbits.TRISC2 = 0 ;
 	TRISCbits.TRISC3 = 0 ;
 
-	PORTCbits.RC2 = 0;
-	PORTCbits.RC4 = 0;
-	PORTCbits.RC3 = 0;
-
+	PORTCbits.RC2 = 0 ;	// Set ports C pins off
+	PORTCbits.RC4 = 0 ;
+	PORTCbits.RC3 = 0 ;
 }
 
-
-void set_green_led(unsigned s) {
-	PORTCbits.RC4 = s;
+void set_green_led ( unsigned s ) {
+	PORTCbits.RC4 = s ;
 }
 
-void set_red_led(unsigned s) {
-	PORTCbits.RC2 = s;
+void set_yellow_led ( unsigned s ) {
+	PORTCbits.RC3 = s ;
 }
 
-void set_yellow_led(unsigned s) {
-	PORTCbits.RC3 = s;
+void set_red_led ( unsigned s ) {
+	PORTCbits.RC2 = s ;
 }
 
-void toggle_green_led() {
+void toggle_green_led (  ) {
 	PORTCbits.RC4 = !PORTCbits.RC4 ;
 }
 
-
-void toggle_yellow_led() {
+void toggle_yellow_led (  ) {
 	PORTCbits.RC3 = !PORTCbits.RC3 ;
 }
 
-void toggle_red_led() {
+void toggle_red_led (  ) {
 	PORTCbits.RC2 = !PORTCbits.RC2 ;
 }
 
-void set_all_leds( unsigned s ) {
-	PORTCbits.RC4 = s;
-	PORTCbits.RC2 = s;
-	PORTCbits.RC3 = s;
+void set_all_leds ( unsigned s ) {
+	PORTCbits.RC4 = s ;
+	PORTCbits.RC2 = s ;
+	PORTCbits.RC3 = s ;
 }
+
+/* Local Variables:    */
+/* mode: c             */
+/* c-basic-offset: 8   */
+/* indent-tabs-mode: t */
+/* End:                */
--- firmware/leds.h
+++ firmware/leds.h
@@ -5,18 +5,16 @@
 #define LED_OFF		 0
 #define LED_AUTO	-1
 
+void setup_leds (  ) ;
 
+void set_green_led ( unsigned ) ;
+void set_red_led ( unsigned ) ;
+void set_yellow_led ( unsigned ) ;
 
-void setup_leds() ;
+void set_all_leds ( unsigned ) ;
 
-void set_green_led(unsigned ) ;
-void set_red_led(unsigned ) ;
-void set_yellow_led(unsigned ) ;
+void toggle_green_led (  ) ;
+void toggle_red_led (  ) ;
+void toggle_yellow_led (  ) ;
 
-void set_all_leds( unsigned s ) ;
-
-void toggle_green_led() ;
-void toggle_red_led() ;
-void toggle_yellow_led() ;
-
-#endif 
+#endif
