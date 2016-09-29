cf93abeee180f4a2daea3aa570a6219505b2edb0
Benoit Plessis <b.plessis@doyousoft.com>
2016-09-30 00:36:22 +0200

    Indent code

--- firmware/i2c.c
+++ firmware/i2c.c
@@ -1,63 +1,54 @@
 #include <xc.h>
 #include "i2c.h"
 
-signed char WriteI2C( unsigned char data_out ) {
+signed char WriteI2C ( unsigned char data_out ) {
 
-	SSP1BUF = data_out;           // write single byte to SSP1BUF
-	if ( SSP1CON1bits.WCOL )      // test if write collision occurred
-		return ( -1 );        // if WCOL bit is set return negative #
+	SSP1BUF = data_out;	// write single byte to SSP1BUF
+	if ( SSP1CON1bits.WCOL )	// test if write collision occurred
+		return ( -1 );	// if WCOL bit is set return negative #
 	else {
-		
-		while( SSP1STATbits.BF );    // wait until write cycle is complete      
-		IdleI2C();                   // ensure module is idle
-	    			
-		if ( SSP1CON2bits.ACKSTAT )  // test for ACK condition received
-	    	 	return -2 ;	     // Return NACK	
-		else 
-			return 0 ;        // Return ACK
+
+		while ( SSP1STATbits.BF ) ;	// wait until write cycle is complete
+		IdleI2C (  );	// ensure module is idle
+
+		if ( SSP1CON2bits.ACKSTAT )	// test for ACK condition received
+			return -2;	// Return NACK
+		else
+			return 0;	// Return ACK
 	}
 }
 
+void OpenI2C ( unsigned char sync_mode, unsigned char slew ) {
 
-void OpenI2C( unsigned char sync_mode, unsigned char slew )
-{
-  
-	  SSP1STAT &= 0x3F;                // power on state 
-	  SSP1CON1 = 0x00;                 // power on state
-	  SSP1CON2 = 0x00;                 // power on state
-	  
-	  SSP1CON1 |= sync_mode;           // select serial mode 
-	  SSP1STAT |= slew;                // slew rate on/off   
+	SSP1STAT &= 0x3F;	// power on state
+	SSP1CON1 = 0x00;	// power on state
+	SSP1CON2 = 0x00;	// power on state
 
-	  SSPADD = 0x13 ;
+	SSP1CON1 |= sync_mode;	// select serial mode
+	SSP1STAT |= slew;	// slew rate on/off
 
-	  SSP1CON1 |= SSPENB;              // enable synchronous serial port 
+	SSPADD = 0x1 ;
+
+	SSP1CON1 |= SSPENB;	// enable synchronous serial port
 }
 
+unsigned char ReadI2C ( void ) {
+	SSP1CON2bits.RCEN = 1;	// enable master for 1 byte reception
+	while ( !SSP1STATbits.BF ) ;	// wait until byte received
 
-unsigned char ReadI2C( void )
-{
-	SSP1CON2bits.RCEN = 1 ;		   // enable master for 1 byte reception
-	while ( !SSP1STATbits.BF );        // wait until byte received  
-
-	return (SSP1BUF) ;
+	return ( SSP1BUF ) ;
 }
 
-void AckI2C( void )
-{
-	SSP1CON2bits.ACKDT = 0;            // set acknowledge bit state for ACK
-	SSP1CON2bits.ACKEN = 1;            // initiate bus acknowledge sequence
+void AckI2C ( void ) {
+	SSP1CON2bits.ACKDT = 0;	// set acknowledge bit state for ACK
+	SSP1CON2bits.ACKEN = 1;	// initiate bus acknowledge sequence
 }
 
-void NotAckI2C( void )
-{
-	SSP1CON2bits.ACKDT = 1;          // set acknowledge bit for not ACK
-	SSP1CON2bits.ACKEN = 1;          // initiate bus acknowledge sequence
+void NotAckI2C ( void ) {
+	SSP1CON2bits.ACKDT = 1;	// set acknowledge bit for not ACK
+	SSP1CON2bits.ACKEN = 1;	// initiate bus acknowledge sequence
 }
 
-void RestartI2C( void )
-{
-	SSP1CON2bits.RSEN = 1;           // initiate bus restart condition
+void RestartI2C ( void ) {
+	SSP1CON2bits.RSEN = 1;	// initiate bus restart condition
 }
-
-
--- firmware/i2c.h
+++ firmware/i2c.h
@@ -1,25 +1,23 @@
 #ifndef I2C_H
 #define I2C_H
 
+#define   MASTER    			0b00001000	/* I2C Master mode */
+#define   SLEW_OFF  			0b10000000	/* Slew rate disabled for 100kHz mode */
+#define   SLEW_ON   			0b00000000	/* Slew rate enabled for 400kHz mode  */
 
-#define   MASTER    			0b00001000     	/* I2C Master mode */
-#define   SLEW_OFF  			0b10000000  	/* Slew rate disabled for 100kHz mode */
-#define   SLEW_ON   			0b00000000  	/* Slew rate enabled for 400kHz mode  */
-
-#define   SSPENB    			0b00100000  	/* Enable serial port and configures SCK, SDO, SDI*/
+#define   SSPENB    			0b00100000	/* Enable serial port and configures SCK, SDO, SDI */
 
 #define StartI2C()  SSP1CON2bits.SEN=1;while(SSP1CON2bits.SEN)
 #define StopI2C()   SSP1CON2bits.PEN=1;while(SSP1CON2bits.PEN)
 #define IdleI2C()   while ((SSP1CON2 & 0x1F) | (SSP1STATbits.R_nW))
 #define CloseI2C()  SSP1CON1 &=0xDF
 
+void OpenI2C ( unsigned char sync_mode, unsigned char slew ) ;
+signed char WriteI2C ( unsigned char data_out ) ;
+unsigned char ReadI2C ( void ) ;
 
-void OpenI2C(  unsigned char sync_mode,  unsigned char slew );
-signed char WriteI2C(  unsigned char data_out );
-unsigned char ReadI2C( void ) ;
-
-void AckI2C( void ) ;
-void NotAckI2C( void ) ;
-void RestartI2C( void ) ;
+void AckI2C ( void ) ;
+void NotAckI2C ( void ) ;
+void RestartI2C ( void ) ;
 
 #endif
