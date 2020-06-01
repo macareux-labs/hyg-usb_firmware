ea731c7c5090e39adfc1b6955675941043d8db1a
Benoit Plessis <benoit.plessis@powerboutique.com>
2020-06-01 23:41:40 +0200

    Add crc check for i2c transfers

--- firmware/si7021A10.c
+++ firmware/si7021A10.c
@@ -5,8 +5,9 @@
 #include "i2c.h"
 #include "si7021A10.h"
 
-signed char get_i2c_word_at ( unsigned char addr, __WORD * value ) {
+signed char get_i2c_word_at ( unsigned char addr, __WORD * value, bool check_crc ) {
 	signed char ret ;
+	uint8_t crc ;
 	ret = 0 ;
 
 	IdleI2C (  ) ;
@@ -27,6 +28,20 @@ signed char get_i2c_word_at ( unsigned char addr, __WORD * value ) {
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 		value->lsb = ReadI2C (  ) ;
+
+#ifdef ENABLE_CRC
+		if ( check_crc ) {
+			AckI2C (  ) ;
+			IdleI2C (  ) ;
+			crc = ReadI2C (  ) ;
+
+			if ( !check_si7021_crc ( (uint8_t*) value, 2, crc ) ) {
+				value->msb = 0xee ;
+				value->lsb = 0xee ;
+			}
+		}
+#endif
+
 		NotAckI2C (  ) ;
 
 	} else {
@@ -44,20 +59,42 @@ signed char get_i2c_word_at ( unsigned char addr, __WORD * value ) {
 
 /* Measure Relative Humidity */
 signed char measure_rh ( __WORD * value ) {
-	return get_i2c_word_at ( 0xE5, value ) ;
+	return get_i2c_word_at ( 0xE5, value, true ) ;
 }
 
 /* Measure Temperature */
 signed char measure_temp ( __WORD * value ) {
-	return get_i2c_word_at ( 0xE3, value ) ;
+	return get_i2c_word_at ( 0xE3, value, true ) ;
 }
 
 /* Measure Relative Humidity and fetch measured temp */
 signed char measure_all ( __WORD * rh, __WORD * temp ) {
-	uint8_t r1 = get_i2c_word_at ( 0xE5, rh ) ;
-	uint8_t r2 = get_i2c_word_at ( 0xE0, temp ) ;
+	uint8_t r1 = get_i2c_word_at ( 0xE5, rh, true ) ;
+	uint8_t r2 = get_i2c_word_at ( 0xE0, temp, false ) ; // No CRC available when fetching temp from relative humidity
 	return r1 && r2 ;
 }
+
+#ifdef ENABLE_CRC
+signed char check_si7021_crc ( uint8_t *values, uint8_t len, uint8_t check ) {
+    unsigned char crc = 0u;
+    char i, j;
+
+    for (i = 0; i < len; i++ ) {
+	    crc ^= values[i];
+	    for (j = 8u; j > 0u; j--)
+	    {
+		    if (crc & 0x80u)
+			    crc = (crc << 1u) ^ 0x131u;
+		    else
+			    crc = (crc << 1u);
+	    }
+    }
+
+    return (crc == check);
+}
+#else
+#define check_si7021_crc(v,l,c) (true)
+#endif
 
 /* Fetch 64bit serial number of sensor */
 uint8_t get_si7021_serial ( si7021serial * serial ) {
@@ -86,6 +123,11 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 		crc = ReadI2C (  ) ;
+
+		if ( !check_si7021_crc ( serial->bytes, 1, crc ) ) {
+			serial->bytes[0] = 0xee ;
+		}
+
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 
@@ -93,6 +135,11 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 		crc = ReadI2C (  ) ;
+
+		if ( !check_si7021_crc ( serial->bytes, 2, crc ) ) {
+			serial->bytes[1] = 0xee ;
+		}
+
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 
@@ -100,6 +147,11 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 		crc = ReadI2C (  ) ;
+
+		if ( !check_si7021_crc ( serial->bytes, 3, crc ) ) {
+			serial->bytes[2] = 0xee ;
+		}
+
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 
@@ -107,6 +159,11 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 		AckI2C (  ) ;
 		IdleI2C (  ) ;
 		crc = ReadI2C (  ) ;
+
+		if ( !check_si7021_crc ( serial->bytes, 4, crc ) ) {
+			serial->bytes[3] = 0xee ;
+		}
+
 		NotAckI2C (  ) ;
 
 		// Read Second DWORD
@@ -135,6 +192,10 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 			AckI2C (  ) ;
 			IdleI2C (  ) ;
 
+			if ( !check_si7021_crc ( serial->bytes+4, 2, crc ) ) {
+				serial->bytes[4] = 0xee ;
+			}
+
 			serial->bytes[6] = ReadI2C (  );	//SNB_1
 			AckI2C (  ) ;
 			IdleI2C (  ) ;
@@ -143,6 +204,10 @@ uint8_t get_si7021_serial ( si7021serial * serial ) {
 			IdleI2C (  ) ;
 			crc = ReadI2C (  ) ;
 			NotAckI2C (  ) ;
+
+			if ( !check_si7021_crc ( serial->bytes+4, 4, crc ) ) {
+				serial->bytes[6] = 0xee ;
+			}
 
 		}
 	}
--- firmware/si7021A10.h
+++ firmware/si7021A10.h
@@ -1,6 +1,8 @@
 #ifndef SENSOR_7021
 #define SENSOR_7021
 
+#define   CRC_POLY			0b100110001	/* CRC Polynome x^8 + x^5 + x^4 + 1 */
+
 typedef union {
 	uint8_t bytes[8] ;
 	struct {
@@ -12,6 +14,14 @@ typedef union {
 signed char measure_rh ( __WORD * ) ;
 signed char measure_temp ( __WORD * ) ;
 signed char measure_all ( __WORD *, __WORD * ) ;
+signed char check_si7021_crc ( uint8_t *, uint8_t, uint8_t ) ;
 
 uint8_t get_si7021_serial ( si7021serial * ) ;
 #endif
+
+/* Local Variables:    */
+/* mode: c             */
+/* c-file-style: "k&r" */
+/* c-basic-offset: 8   */
+/* indent-tabs-mode: t */
+/* End:                */
